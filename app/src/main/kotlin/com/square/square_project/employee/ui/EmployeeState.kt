b098a3dbd583b55dd70606289e0efb3528b83488
kennyli <kennyli138@gmail.com>
2021-07-31 16:04:12 -0500

    Implemented EmployeeViewModel.kt

--- app/src/main/kotlin/com/square/square_project/employee/ui/EmployeeState.kt
+++ app/src/main/kotlin/com/square/square_project/employee/ui/EmployeeState.kt
@@ -1,10 +1,14 @@
 package com.square.square_project.employee.ui
 
 import com.square.square_project.employee.model.Employee
+import com.square.square_project.utils.Visibility
 
 sealed class EmployeeState {
   object Noop: EmployeeState()
   data class ListItem(
     val employees: List<Employee>
   ) : EmployeeState()
+  data class ProgressVisibility(
+    val visibility: Visibility
+  ): EmployeeState()
 }
--- app/src/main/kotlin/com/square/square_project/employee/ui/EmployeeViewModel.kt
+++ app/src/main/kotlin/com/square/square_project/employee/ui/EmployeeViewModel.kt
@@ -2,9 +2,14 @@ package com.square.square_project.employee.ui
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.square.square_project.employee.data.EmployeeUseCaseImpl
+import com.google.android.material.snackbar.Snackbar
+import com.square.square_project.R
+import com.square.square_project.employee.data.EmployeeUseCase
 import com.square.square_project.employee.model.Employee
+import com.square.square_project.employee.model.GetEmployeesResponse
+import com.square.square_project.utils.SnackbarModel
 import com.square.square_project.utils.StateEvent
+import com.square.square_project.utils.Visibility
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
@@ -15,12 +20,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class EmployeeViewModel @Inject constructor(
-  private val employeeUseCase: EmployeeUseCaseImpl
+  private val employeeUseCase: EmployeeUseCase
 ) : ViewModel() {
   private val stateEventObs =
     PublishSubject.create<StateEvent<EmployeeState, EmployeeEvent>>().toSerialized()
 
-  private val employees: MutableList<Employee> = mutableListOf()
+  private var employees: MutableList<Employee> = mutableListOf()
 
   /**
    * emit State that should persist on screen upon foreground from backgrounding
@@ -63,13 +68,102 @@ class EmployeeViewModel @Inject constructor(
    * Initial data load to populate UI
    */
   private fun onLoad(): Observable<StateEvent<EmployeeState, EmployeeEvent>> {
-    return Observable.just(StateEvent(EmployeeState.Noop, EmployeeEvent.Noop))
+    return if (employees.isNotEmpty()) {
+      Observable.just(
+        StateEvent(
+          EmployeeState.ListItem(
+            employees = employees
+          ),
+          EmployeeEvent.Noop
+        )
+      )
+    } else {
+      onGetEmployees()
+    }
   }
 
   /**
    * Get latest weather conditions of the present and next 5 days, and display std dev
    */
   private fun onGetEmployees(): Observable<StateEvent<EmployeeState, EmployeeEvent>> {
-    return Observable.just(StateEvent(EmployeeState.Noop, EmployeeEvent.Noop))
+    return employeeUseCase.getEmployees()
+      .flatMapObservable { response ->
+        when(response) {
+          is GetEmployeesResponse.Employees -> {
+            employees = response.employees.toMutableList()
+            Observable.just(
+              StateEvent(
+                EmployeeState.ListItem(
+                  employees = response.employees
+                ),
+                EmployeeEvent.Noop
+              ),
+              StateEvent(
+                EmployeeState.ProgressVisibility(visibility = Visibility.GONE),
+                EmployeeEvent.Noop
+              )
+            )
+          }
+          is GetEmployeesResponse.NetworkError -> {
+            Observable.just(
+              StateEvent(
+                EmployeeState.Noop,
+                EmployeeEvent.Snackbar(
+                  vm = SnackbarModel(
+                    messageResId = R.string.get_employees_network_error,
+                    duration = Snackbar.LENGTH_LONG
+                  )
+                )
+              ),
+              StateEvent(
+                EmployeeState.ProgressVisibility(visibility = Visibility.GONE),
+                EmployeeEvent.Noop
+              )
+            )
+          }
+          is GetEmployeesResponse.NotFound -> {
+            Observable.just(
+              StateEvent(
+                EmployeeState.Noop,
+                EmployeeEvent.Snackbar(
+                  vm = SnackbarModel(
+                    messageResId = R.string.get_employees_not_found_error,
+                    duration = Snackbar.LENGTH_LONG
+                  )
+                )
+              ),
+              StateEvent(
+                EmployeeState.ProgressVisibility(visibility = Visibility.GONE),
+                EmployeeEvent.Noop
+              )
+            )
+          }
+          is GetEmployeesResponse.UnknownError -> {
+            Observable.just(
+              StateEvent(
+                EmployeeState.Noop,
+                EmployeeEvent.Snackbar(
+                  vm = SnackbarModel(
+                    messageResId = R.string.get_employees_unknown_error,
+                    duration = Snackbar.LENGTH_LONG
+                  )
+                )
+              ),
+              StateEvent(
+                EmployeeState.ProgressVisibility(visibility = Visibility.GONE),
+                EmployeeEvent.Noop
+              )
+            )
+          }
+        }
+      }.startWith (
+        Observable.just(
+          StateEvent(
+            EmployeeState.ProgressVisibility(
+              visibility = Visibility.VISIBLE
+            ),
+            EmployeeEvent.Noop
+          ))
+      )
   }
 }
